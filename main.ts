a4d14b2144a08fa0ec42f63b46b0f37c0627888d
zhuyu <zhuyu@elecfreaks.com>
2025-02-14 10:53:35 +0800

    nomsg

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 */
 //% color=#00B1ED  icon="\uf005" block="Petal_Base" blockId="Petal_Base"
 namespace petal {
-    export enum DigitalRJPin {
+    export enum DigitalPort {
         //% block="J1"
         J1,
         //% block="J2"
@@ -13,90 +13,142 @@ namespace petal {
         //% block="J4"
         J4
     }
-    export enum AnalogRJPin {
+    export enum AnalogPort {
         //% block="J1"
         J1,
         //% block="J2"
         J2
     }
 
-    ///////////////////////////////////////////////////////RJpin_to_pin
-    function RJpin_to_analog(Rjpin: AnalogRJPin): any {
+    export enum SwitchState {
+        //% block="Open"
+        Open,
+        //% block="Off"
+        Off
+    }
+
+    export function portToAnalogPin(port: AnalogPort): any {
         let pin = AnalogPin.P1
-        switch (Rjpin) {
-            case AnalogRJPin.J1:
+        switch (port) {
+            case AnalogPort.J1:
                 pin = AnalogPin.P1
                 break;
-            case AnalogRJPin.J2:
+            case AnalogPort.J2:
                 pin = AnalogPin.P2
                 break;
         }
         return pin
     }
-    function RJpin_to_digital(Rjpin: DigitalRJPin): any {
+
+    export function portToDigitalPin(port: DigitalPort): any {
         let pin = DigitalPin.P1
-        switch (Rjpin) {
-            case DigitalRJPin.J1:
+        switch (port) {
+            case DigitalPort.J1:
                 pin = DigitalPin.P1
                 break;
-            case DigitalRJPin.J2:
+            case DigitalPort.J2:
                 pin = DigitalPin.P2
                 break;
-            case DigitalRJPin.J3:
+            case DigitalPort.J3:
                 pin = DigitalPin.P13
                 break;
-            case DigitalRJPin.J4:
+            case DigitalPort.J4:
                 pin = DigitalPin.P15
                 break;
         }
         return pin
     }
 
-    //% blockId=button block="Button sensor %Rjpin is pressed"
+    //% blockId=button block="Button sensor %port is pressed"
     //% color=#EA5532 weight=100
-    export function buttonRead(Rjpin: DigitalRJPin): boolean {
-        let pin = RJpin_to_digital(Rjpin)
+    export function buttonRead(port: DigitalPort): boolean {
+        let pin = portToDigitalPin(port)
         pins.setPull(pin, PinPullMode.PullUp)
         return pins.digitalReadPin(pin) == 1
     }
 
-    //% blockId=hall block="hall sensor %Rjpin is attracted"
+    //% blockId=hall block="Hall sensor %port is attracted"
     //% color=#EA5532 weight=95
-    export function hallRead(Rjpin: DigitalRJPin): boolean {
-        let pin = RJpin_to_digital(Rjpin)
+    export function hallRead(port: DigitalPort): boolean {
+        let pin = portToDigitalPin(port)
         pins.setPull(pin, PinPullMode.PullUp)
         return pins.digitalReadPin(pin) == 1
     }
 
-    //% blockId=PIR block="PIR sensor %Rjpin detects motion"
+    //% blockId=pir block="PIR sensor %port detects motion"
     //% color=#EA5532 weight=90
-    export function PIRRead(Rjpin: DigitalRJPin): boolean {
-        let pin = RJpin_to_digital(Rjpin)
+    export function pirRead(port: DigitalPort): boolean {
+        let pin = portToDigitalPin(port)
         pins.setPull(pin, PinPullMode.PullUp)
         return pins.digitalReadPin(pin) == 1
     }
 
-    //% blockId=buzzer block="buzzer sensor %Rjpin play ring tone (Hz)|%note=device_note" blockGap=8
+    //% blockId=buzzer block="Buzzer sensor %port play ring tone (Hz)|%note=device_note" blockGap=8
     //% help=music/ring-tone
     //% color=#EA5532 weight=85
     //% parts="headphone"
     //% useEnumVal=1
-    export function buzzerWrite(Rjpin: AnalogRJPin, frequency: number): void {
-        let pin = RJpin_to_analog(Rjpin);
+    export function buzzerWrite(port: AnalogPort, frequency: number): void {
+        let pin = portToAnalogPin(port);
         let periodUs = 1000000 / frequency;
+        pins.analogWritePin(pin, 0);
         pins.analogSetPeriod(pin, Math.idiv(periodUs, 2));
         let dutyCycle = 512;
         pins.analogWritePin(pin, dutyCycle);
-        periodUs = 1000000 / frequency;
-        pins.analogSetPeriod(pin, Math.idiv(periodUs, 2));
-        dutyCycle = 512;
-        pins.analogWritePin(pin, dutyCycle);
+
+
+        // periodUs = 1000000 / frequency;
+        // pins.analogSetPeriod(pin, Math.idiv(periodUs, 2));
+        // dutyCycle = 512;
+        // pins.analogWritePin(pin, dutyCycle);
     }
 
-    //% blockId="trimpot" block="Trimpot sensor %Rjpin analog value"
+    //% blockId="trimpot" block="Trimpot sensor %port analog value"
     //% color=#E2C438 weight=40
-    export function trimpot(Rjpin: AnalogRJPin): number {
-        let pin = RJpin_to_analog(Rjpin)
+    export function trimpotRead(port: AnalogPort): number {
+        let pin = portToAnalogPin(port)
         return pins.analogReadPin(pin)
+    }
+
+    //% blockId="noise" block="Noise sensor %port analog value"
+    //% color=#E2C438 weight=35
+    export function noiseRead(port: AnalogPort): number {
+        let pin = portToAnalogPin(port)
+        return pins.analogReadPin(pin)
+    }
+
+    //% blockId="photocell" block="Photocell sensor %port light intensity(lux)"
+    //% color=#E2C438 weight=35
+    export function photocellRead(port: AnalogPort): number {
+        let pin = portToAnalogPin(port)
+        let voltage = 0
+        for (let index = 0; index < 100; index++) {
+            voltage = voltage + pins.analogReadPin(pin)
+        }
+        voltage = voltage / 100
+        if (voltage < 200) {
+            voltage = Math.map(voltage, 0, 200, 0, 1600)
+        }
+        else {
+            voltage = Math.map(voltage, 200, 1023, 1600, 14000)
+        }
+        if (voltage < 0) {
+            voltage = 0
+        }
+        return Math.round(voltage)
+    }
+
+    //% blockId="redled" block="Red led sensor %port %state"
+    //% color=#E2C438 weight=40
+    export function redLedWritr(port: DigitalPort,state:SwitchState): void {
+        let pin = portToDigitalPin(port)
+        switch (state) {
+            case SwitchState.Open:
+                pins.digitalWritePin(pin, 1)
+                break;
+            case SwitchState.Off:
+                pins.digitalWritePin(pin, 0)
+                break;
+        }
     }
 }
